1ff9970a074528a19777bf228cc4c36e790b008b
KMsojib <kawsarmahmud822@gmail.com>
2025-08-03 17:44:06 +0600

    Image Transformations

--- OpenCV with Python/Section - 1 - Basics/Image Transformations.py
+++ OpenCV with Python/Section - 1 - Basics/Image Transformations.py
@@ -1,11 +1,11 @@
 import cv2 as cv
 import numpy as np
 
-# Read image
+## Read image
 img = cv.imread('D:\Computer Vision Library Tutorials and Projects Collection\Computer-Vision\OpenCV with Python\Resources\Photos\cats.jpg')
 cv.imshow('Cat', img)
 
-# Function for Image Translation
+## Function for Image Translation
 
 """ 
 -x --> left
@@ -13,16 +13,16 @@ cv.imshow('Cat', img)
 +x --> Right
 +y --> Down
 """
-# def imgTranslation(image, x, y):
-#     transMat = np.float32([[1,0,x],[0,1,y]])
-#     dimensions = (image.shape[1],image.shape[0])
-#     return cv.warpAffine(image, transMat,dimensions)
+def imgTranslation(image, x, y):
+    transMat = np.float32([[1,0,x],[0,1,y]])
+    dimensions = (image.shape[1],image.shape[0])
+    return cv.warpAffine(image, transMat,dimensions)
 
-# tranlated = imgTranslation(img, 100,100) # (-100,-100),(-100,100),(100,-100)
-# cv.imshow("Translated Image : ", tranlated)
+tranlated = imgTranslation(img, 100,100) # (-100,-100),(-100,100),(100,-100)
+cv.imshow("Translated Image : ", tranlated)
 
 
-# Image Rotation
+## Image Rotation
 """ 
     * Image is input & the rotation angel, the point about which to rotate
     if case:
@@ -36,20 +36,20 @@ cv.imshow('Cat', img)
     Return :
         Applies the roation to the iamge using OpenCv's warpAffine function & return
 """
-# def Rotated(img, angle, rotPoint=None):
-#     (height, weight) = img.shape[:2]
+def Rotated(img, angle, rotPoint=None):
+    (height, weight) = img.shape[:2]
     
-#     if rotPoint is None:
-#         rotPoint = (weight//2,height//2)
+    if rotPoint is None:
+        rotPoint = (weight//2,height//2)
         
-#     rotMat = cv.getRotationMatrix2D(rotPoint,angle,1.0)
-#     dimensions = (weight,height)
+    rotMat = cv.getRotationMatrix2D(rotPoint,angle,1.0)
+    dimensions = (weight,height)
     
-#     return cv.warpAffine(img,rotMat,dimensions)
+    return cv.warpAffine(img,rotMat,dimensions)
 
 
-# Rotated = Rotated(img, -90)  # Rotate by 45 degrees
-# cv.imshow("Rotated Image : ", Rotated)
+Rotated = Rotated(img, -90)  # Rotate by 45 degrees
+cv.imshow("Rotated Image : ", Rotated)
 
 
 # Image Resizing
@@ -59,6 +59,15 @@ def Resized(image, scale=0.5):
 
 resized = Resized(img, 0.5)  # Resize to 50% of original size
 cv.imshow("Resized Image : ", resized)
+
+
+# flip Image
+flip = cv.flip(img, 1)  # 0 for vertical, 1 for horizontal, -1 for both
+cv.imshow("Flipped Image : ", flip)
+
+# Crop image
+cropped = img[200:300, 100:200]  # Crop a region of interest (ROI)
+cv.imshow("Cropped Image : ", cropped)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
